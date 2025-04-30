9773ecb4eefde5e6e07f6ffbb0e18d8e311668a5
Leslie Cruz <128759639+LeslieMayer01@users.noreply.github.com>
2025-04-29 20:02:39 -0500

    Revert "Agregar multiplicación y división"

--- calculadora.js
+++ calculadora.js
@@ -5,12 +5,3 @@ function sumar(a, b) {
   function restar(a, b) {
     return a - b;
   }
-
-  function multiplicar(a, b) {
-    return a * b;
-  }
-  
-  function dividir(a, b) {
-    if (b === 0) throw new Error("División por cero");
-    return a / b;
-  }
